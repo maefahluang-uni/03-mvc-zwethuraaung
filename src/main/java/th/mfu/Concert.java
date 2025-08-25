8d8c7393573505c2f70362106efae7577b91ce4d
Zwe Thura Aung <6731503097@lamduan.mfu.ac.th>
2025-08-25 09:23:38 +0000

    lab done

--- src/main/java/th/mfu/Concert.java
+++ src/main/java/th/mfu/Concert.java
@@ -4,16 +4,65 @@ import java.util.Date;
 
 public class Concert {
 
-    //TODO: add attributes
+    // Attributes
+    private int id;
+    private String title;
+    private String performer;
+    private Date date;
+    private String description;
 
+    // Default constructor
     public Concert() {
     }
+
+    // Constructor with title and description
     public Concert(String title, String description) {
-        //TODO: set attributes
-
+        this.title = title;
+        this.description = description;
     }
- 
-    //TODO: add getters and setters for all attributes
-    // You can use Source action/generate getter setter function
 
+    // Getter and Setter for id
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Getter and Setter for title
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    // Getter and Setter for performer
+    public String getPerformer() {
+        return performer;
+    }
+
+    public void setPerformer(String performer) {
+        this.performer = performer;
+    }
+
+    // Getter and Setter for date
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    // Getter and Setter for description
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
